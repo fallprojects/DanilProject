8710473821199b2f97154346ede5d8dc7f6a1e3a
exel14 <Showmetheway220@gmail.com>
2020-11-09 19:35:15 +0600

    create new model News

--- Football/api/models.py
+++ Football/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    CATEGORY = (
+        ('EPL','EPL'),
+        ('Bundesliga','Bundesliga'),
+        ('La Liga','La Liga'),
+        ('Seria A','Seria A'),
+        ('RPL','RPL'),
+    )
+    name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=1000,null=True)
+    category = models.CharField(max_length=100,null=True,choices=CATEGORY)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
