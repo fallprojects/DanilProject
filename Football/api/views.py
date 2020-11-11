9b9da7115023be8c4699e66418f5948b01ffe8a6
exel14 <Showmetheway220@gmail.com>
2020-11-11 19:29:37 +0600

    add new page(news,games,teams)

--- Football/api/views.py
+++ Football/api/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render,redirect
 from .models import *
 
 def home_page(request):
+    games = Games.objects.all()
     news = News.objects.all()
-    context = {'news':news}
+    context = {'news':news,'games':games}
     return render(request,'api/home.html',context)
+
+def news_page(request):
+    context = {}
+    return render(request,'api/news.html',context)
+
+def games_page(request):
+    context = {}
+    return render(request,'api/games.html',context)
+
+def teams_page(request):
+    teams = Teams.objects.all()
+    context = {'teams':teams}
+    return render(request,'api/teams.html',context)
