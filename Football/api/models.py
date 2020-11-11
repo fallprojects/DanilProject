65317ce124121d69a5f4b74109939c1d231d1ad1
exel14 <Showmetheway220@gmail.com>
2020-11-11 19:30:51 +0600

    add new models(Tag,Games,Teams)

--- Football/api/models.py
+++ Football/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Tag(models.Model):
+    name=models.CharField(max_length=100,null=True)
+
 class News(models.Model):
     CATEGORY = (
         ('EPL','EPL'),
@@ -12,4 +15,28 @@ class News(models.Model):
     description = models.CharField(max_length=1000,null=True)
     category = models.CharField(max_length=100,null=True,choices=CATEGORY)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+class Games(models.Model):
+    name_of_game = models.CharField(max_length=100,null=True)
+    time_of_game = models.DateTimeField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name_of_game
+
+class Teams(models.Model):
+    position = models.IntegerField(max_length=50,null=True)
+    name = models.CharField(max_length=50,null=True)
+    matches = models.IntegerField(max_length=50,null=True)
+    points = models.IntegerField(max_length=50,null=True)
+    goals_scored = models.IntegerField(max_length=50,null=True)
+    goals_conceded = models.IntegerField(max_length=50,null=True)
+    tag = models.ManyToManyField(Tag,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
 
