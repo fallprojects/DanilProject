044607c6693431472f026e203bf655f61d231753
exel14 <Showmetheway220@gmail.com>
2020-11-18 16:31:48 +0600

    add new view

--- Football/api/views.py
+++ Football/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from .filters import TableFilterSet
 from .models import *
 
 def home_page(request):
@@ -16,16 +17,26 @@ def games_page(request):
     context = {'game':game}
     return render(request,'api/games.html',context)
 
-def about_game_page(request,pk):
+def about_game_page(request):
     context = {}
     return render(request,'api/about_game.html',context)
 
 def table_page(request):
-    team = Teams.objects.all()
-    context = {'team':team}
+    team = Teams.objects.all().order_by('-points')
+    filterset= TableFilterSet(request.GET,queryset=team)
+    team = filterset.qs
+    context = {'team':team,'filterset':filterset}
     return render(request,'api/table.html',context)
 
 def team_page(request,pk):
-    command = Players.objects.all()
-    context = {'command':command}
+    team = Teams.objects.get(id=pk)
+    command = team.players_set.all()
+    context = {'command':command,'team':team}
     return render(request,'api/team.html',context)
+
+def rates_page(request,pk):
+    rates = Rates.objects.get(id=pk)
+    if rates.results == 'draw':
+        rates.money = rates.money * 2
+    context = {'rates':rates}
+    return render(request,'api/rates.html',context)
